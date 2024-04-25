c78ea67f7acedb93349f8c1bfdb8e5c5b826f60f
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-04-25 19:17:36 +0200

    Add exceptions handling

--- main.py
+++ main.py
@@ -8,33 +8,33 @@ from scrapper import find_flyby
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    with open("subscribers.json", encoding="utf-8") as file:
-        data = json.load(file)
+    try:
+        with open("subscribers.json", encoding="utf-8") as file:
+            data = json.load(file)
 
-    subscribers = data["subscribers"]
-    notification = ""
+        subscribers = data["subscribers"]
 
-    for subscriber in subscribers:
-        subscriber = Subscriber(
-            name=subscriber["name"],
-            email=subscriber["email"],
-            object=subscriber["object"],
-            lat=subscriber["loc"]["lat"],
-            long=subscriber["loc"]["long"]
-        )
+        for subscriber in subscribers:
+            subscriber = Subscriber(
+                name=subscriber["name"],
+                email=subscriber["email"],
+                object=subscriber["object"],
+                lat=subscriber["loc"]["lat"],
+                long=subscriber["loc"]["long"]
+            )
 
-        flyby_data = find_flyby(
-            satellite_id=subscriber.object,
-            latitude=subscriber.lat,
-            longitude=subscriber.long
-        )
+            flyby_data = find_flyby(
+                satellite_id=subscriber.object,
+                latitude=subscriber.lat,
+                longitude=subscriber.long
+            )
 
-        if flyby_data:
-            notification = Notification(flyby_data=flyby_data)
-        try:
-            notification.send_mail_to(subscriber)
-        except SMTPAuthenticationError as e:
-            logging.error(e)
-        except SMTPRecipientsRefused as e:
-            logging.error(e)
-
+            if flyby_data:
+                notification = Notification(flyby_data=flyby_data)
+                notification.send_mail_to(subscriber)
+    except FileNotFoundError:
+        logging.error("File not found!")
+    except SMTPAuthenticationError as error:
+        logging.error(error)
+    except SMTPRecipientsRefused as error:
+        logging.error(error)
--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 
 
 logging.basicConfig(level=logging.INFO)
@@ -20,36 +21,40 @@ def find_flyby(satellite_id: str, latitude: str, longitude: str) -> dict:
         dict: flyby information
     """
 
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Firefox(options=options)
+    try:
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
 
-    webpage = f'https://heavens-above.com/PassSummary.aspx?satid={satellite_id}&lat={latitude}8&lng={longitude}&loc=Unnamed&alt=0&tz=CET'
-    driver.get(webpage)
+        webpage = f'https://heavens-above.com/PassSummary.aspx?satid={satellite_id}&lat={latitude}8&lng={longitude}&loc=Unnamed&alt=0&tz=CET'
+        driver.get(webpage)
 
-    row = driver.find_element(By.CLASS_NAME, "clickableRow").text
-    driver.quit()
+        row = driver.find_element(By.CLASS_NAME, "clickableRow").text
+        driver.quit()
 
-    row = row.split(' ')
-    flyby_brightness = row[2]
-    flyby_start_time = row[3]
-    flyby_start_altitude = row[4]
-    flyby_max_time = row[6]
-    flyby_max_altitude = row[7]
-    flyby_end_time = row[9]
-    flyby_end_altitude = row[10]
+        row = row.split(' ')
+        flyby_brightness = row[2]
+        flyby_start_time = row[3]
+        flyby_start_altitude = row[4]
+        flyby_max_time = row[6]
+        flyby_max_altitude = row[7]
+        flyby_end_time = row[9]
+        flyby_end_altitude = row[10]
 
-    parsed_flyby_start_time = datetime.strptime(flyby_start_time, "%H:%M:%S")
-    parsed_flyby_end_time = datetime.strptime(flyby_end_time, "%H:%M:%S")
-    flyby_duration = str(parsed_flyby_end_time - parsed_flyby_start_time)
+        parsed_flyby_start_time = datetime.strptime(flyby_start_time, "%H:%M:%S")
+        parsed_flyby_end_time = datetime.strptime(flyby_end_time, "%H:%M:%S")
+        flyby_duration = str(parsed_flyby_end_time - parsed_flyby_start_time)
 
-    return {
-        "flyby_brightness": flyby_brightness,
-        "flyby_start_time": flyby_start_time,
-        "flyby_start_altitude": flyby_start_altitude,
-        "flyby_max_time": flyby_max_time,
-        "flyby_max_altitude": flyby_max_altitude,
-        "flyby_end_time": flyby_end_time,
-        "flyby_end_altitude": flyby_end_altitude,
-        "flyby_duration": flyby_duration
-    }
+        return {
+            "flyby_brightness": flyby_brightness,
+            "flyby_start_time": flyby_start_time,
+            "flyby_start_altitude": flyby_start_altitude,
+            "flyby_max_time": flyby_max_time,
+            "flyby_max_altitude": flyby_max_altitude,
+            "flyby_end_time": flyby_end_time,
+            "flyby_end_altitude": flyby_end_altitude,
+            "flyby_duration": flyby_duration
+        }
+
+    except WebDriverException as error:
+        logging.error("Webpage not reached")
