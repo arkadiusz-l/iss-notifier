bec725b1604fca58498b911f4c7ce4a652c42073
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-04-25 18:43:51 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from smtplib import SMTPAuthenticationError
+import json
+from smtplib import SMTPAuthenticationError, SMTPRecipientsRefused
 import logging
 from notification import Notification
 from subscriber import Subscriber
@@ -7,15 +8,33 @@ from scrapper import find_flyby
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    subscribers = [
-        Subscriber('Name_of_subscriber', 'email@ofsubscriber.com'),
-    ]
+    with open("subscribers.json", encoding="utf-8") as file:
+        data = json.load(file)
 
-    flyby_data = find_flyby(satellite_id="25544", latitude="52.2320", longitude="21.0067")
-    if flyby_data:
-        notification = Notification(flyby_data=flyby_data)
-        for subscriber in subscribers:
-            try:
-                notification.send_mail_to(subscriber)
-            except SMTPAuthenticationError as e:
-                logging.error(e)
+    subscribers = data["subscribers"]
+    notification = ""
+
+    for subscriber in subscribers:
+        subscriber = Subscriber(
+            name=subscriber["name"],
+            email=subscriber["email"],
+            object=subscriber["object"],
+            lat=subscriber["loc"]["lat"],
+            long=subscriber["loc"]["long"]
+        )
+
+        flyby_data = find_flyby(
+            satellite_id=subscriber.object,
+            latitude=subscriber.lat,
+            longitude=subscriber.long
+        )
+
+        if flyby_data:
+            notification = Notification(flyby_data=flyby_data)
+        try:
+            notification.send_mail_to(subscriber)
+        except SMTPAuthenticationError as e:
+            logging.error(e)
+        except SMTPRecipientsRefused as e:
+            logging.error(e)
+
