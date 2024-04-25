814a3462e10f1902ff52f9dcf513875890e1838c
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-04-25 20:42:12 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -11,30 +11,32 @@ if __name__ == '__main__':
     try:
         with open("subscribers.json", encoding="utf-8") as file:
             data = json.load(file)
-
-        subscribers = data["subscribers"]
-
-        for subscriber in subscribers:
-            subscriber = Subscriber(
-                name=subscriber["name"],
-                email=subscriber["email"],
-                object=subscriber["object"],
-                lat=subscriber["loc"]["lat"],
-                long=subscriber["loc"]["long"]
-            )
-
-            flyby_data = find_flyby(
-                satellite_id=subscriber.object,
-                latitude=subscriber.lat,
-                longitude=subscriber.long
-            )
-
-            if flyby_data:
-                notification = Notification(flyby_data=flyby_data)
-                notification.send_mail_to(subscriber)
     except FileNotFoundError:
         logging.error("File not found!")
-    except SMTPAuthenticationError as error:
-        logging.error(error)
-    except SMTPRecipientsRefused as error:
-        logging.error(error)
+
+    subscribers = data["subscribers"]
+    notification = ""
+
+    for subscriber in subscribers:
+        subscriber = Subscriber(
+            name=subscriber["name"],
+            email=subscriber["email"],
+            object=subscriber["object"],
+            lat=subscriber["loc"]["lat"],
+            long=subscriber["loc"]["long"]
+        )
+
+        flyby_data = find_flyby(
+            satellite_id=subscriber.object,
+            latitude=subscriber.lat,
+            longitude=subscriber.long
+        )
+
+        if flyby_data:
+            notification = Notification(flyby_data=flyby_data)
+        try:
+            notification.send_mail_to(subscriber)
+        except SMTPAuthenticationError as e:
+            logging.error(e)
+        except SMTPRecipientsRefused as e:
+            logging.error(e)
